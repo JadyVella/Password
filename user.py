06789c8ea3c63e1ac9b7c39eb9e8c95e95f4e248
Jady <felixouma379@gmail.com>
2020-01-27 10:11:31 +0300

    Create a tearDown method in user_test.py that does clean up after each test case has run

---
+++ user.py
@@ -0,0 +1,30 @@
+class User:
+     """
+     Class that generates new instances of users.
+     """
+
+     user_list = [] # Empty contact list
+
+     def __init__(self,login_username,password):
+
+       # docstring removed for simplicity
+
+         self.login_username = login_username
+         self.password = password
+
+
+     def save_user(self):
+         '''
+         save_user method saves user into the list
+         '''
+
+         User.user_list.append(self)
+
+
+
+
+
+    
+
+
+    
