390137318d6c056a0909bf01bfa148d0b2cf336d
Jady <felixouma379@gmail.com>
2020-01-27 12:46:55 +0300

    - Create a method called test_display_all_users and checks if they will return user list in user_test.py - create a method called display_users that returns user list in user.py

--- user.py
+++ user.py
@@ -52,6 +52,16 @@ class User:
         
         return False
 
+    @classmethod
+    def display_users(cls):
+        '''
+        method that returns the user list
+        '''
+
+        return cls.user_list
+
+
+
 
 
 
