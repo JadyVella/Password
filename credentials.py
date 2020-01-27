1fcab6bbf6795cff985b5a4b1bb267c160fbbbbf
Jady <felixouma379@gmail.com>
2020-01-27 13:07:45 +0300

    - Created files named, credentials_test.py, credentials.py and run.py in Password-Cracker folder

---
+++ credentials.py
@@ -0,0 +1,10 @@
+# class Credentials:
+#     """
+#     Class that generates new instances of credentials
+#     """
+
+#     pass
+
+#     def __init__(self, first_name, last_name, phone_number, password, email):
+
+
